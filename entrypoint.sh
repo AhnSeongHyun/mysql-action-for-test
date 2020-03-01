b28353640d8c24cd32349b9822aa1ddd421d2793
ahnseonghyun <sh84.ahn@gmail.com>
2020-03-01 23:49:27 +0900

    Fix test

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ if [ -n "$INPUT_MYSQL_DATABASE" ]; then
 fi
 
 cmd="$cmd -e MYSQL_ALLOW_EMPTY_PASSWORD=true"
-cmd="$cmd -d -p 3306:3306 mysql:$INPUT_VERSION"
+cmd="$cmd -d -p 3306:3306 mysql:$INPUT_VERSION --port=3306"
 cmd="$cmd --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci"
 echo "$cmd"
 sh -c "$cmd"
