7e060d206ca8b2cd19c428ee0927b613be845d06
ahnseonghyun <sh84.ahn@gmail.com>
2020-03-02 00:11:28 +0900

    Fix test

--- tests/test_conn.py
+++ tests/test_conn.py
@@ -5,7 +5,6 @@ def test_conn():
     connection = pymysql.connect(host='localhost',
                                  port=3306,
                                  user='root',
-                                 password='',
                                  db='',
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
